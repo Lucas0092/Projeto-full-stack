f6ce073add0f643ff01ac3e677afa66082f546f5
Lucas0092 <lucasumc@outlook.com>
2021-10-15 18:12:40 -0300

    criação componente nova pagina de tasks

--- projetoFront/src/app/components/read-all/read-all.component.ts
+++ projetoFront/src/app/components/read-all/read-all.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Todo } from 'src/app/models/todo';
 import { TodoService } from 'src/app/services/todo.service';
 
@@ -14,7 +15,10 @@ export class ReadAllComponent implements OnInit {
   list: Todo[] = [];
   listFinished: Todo[] = [];
 
-  constructor(private service: TodoService) { }
+  constructor(
+    private service: TodoService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.findAll();
@@ -24,7 +28,7 @@ export class ReadAllComponent implements OnInit {
 
   findAll(): void {
     this.service.findAll().subscribe((resposta) => {
-      resposta.forEach(todo => {
+      resposta.forEach((todo) => {
         if(todo.finalizado){
           this.listFinished.push(todo);
         }else{
@@ -33,6 +37,16 @@ export class ReadAllComponent implements OnInit {
       })
      this.closed = this.listFinished.length
     })
+  }
+
+  finalizarTask(item: Todo): void{
+    item.finalizado = true;
+    this.service.update(item).subscribe(() =>{
+      this.service.message("Task finalizada com sucesso!");
+      this.list = this.list.filter(todo => todo.id !== item.id);
+      this.closed++
+    })
+    
   }
 
   delete(id: any):void{
@@ -44,4 +58,7 @@ export class ReadAllComponent implements OnInit {
     })
   }
 
+  finalizados(): void{
+    this.router.navigate(["finalizados"])
+  }
 }
