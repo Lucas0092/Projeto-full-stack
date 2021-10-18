66c786dd12f26aa55f42b1f36d5065e648e86e43
Lucas0092 <lucasumc@outlook.com>
2021-10-18 15:49:22 -0300

    atualização das tarefas

--- demo/src/main/java/com/projetofullstack/demo/service/DbService.java
+++ demo/src/main/java/com/projetofullstack/demo/service/DbService.java
@@ -2,8 +2,6 @@ package com.projetofullstack.demo.service;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,15 +19,18 @@ public class DbService {
 	public void InstanciaBaseDeDados() throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		Todo t1 = new Todo(null, "Estudar", "Estudar Spring Boot 2 e Angular 11", sdf.parse("25/03/2022"), false);
-		Todo t2 = new Todo(null, "Ler", "Ler livro de desenvolvimento pessoal", sdf.parse("22/03/2021"), true);
-        Todo t3 = new Todo(null, "Exercicios", "Praticar exercicios fisicos", sdf.parse("21/03/2022"), false);
-        Todo t4 = new Todo(null, "Meditar", "Meditar durante 30 minutos pela manhã", sdf.parse("27/03/2022"), false);
-        Todo t5 = new Todo(null, "Correr", "Correr 20 kilometros ", sdf.parse("27/09/2022"), false);
-        Todo t6 = new Todo(null, "Trabalho", "Trabalho da faculdade para apresentar", sdf.parse("27/05/2022"), true);
+		Todo t1 = new Todo(null, "Estudar", "Estudar mais sobre TI", sdf.parse("31/12/2021"), true);
+		Todo t2 = new Todo(null, "Jogar", "Jogar com meu time de futebol", sdf.parse("12/12/2021"), true);
+        Todo t3 = new Todo(null, "Controlar", "Fazer exames regularmente", sdf.parse("30/03/2022"), false);
+        Todo t4 = new Todo(null, "Projeto", "Completar o projeto dos afazeres", sdf.parse("20/10/2021"), false);
+        Todo t5 = new Todo(null, "Correr", "Correr 10 kilometros ", sdf.parse("31/12/2021"), true);
+        Todo t6 = new Todo(null, "Emprego", "Arrumar emprego na área de TI", sdf.parse("30/01/2022"), false);
+        Todo t7 = new Todo(null, "Sonho", "Comprar meu sitio", sdf.parse("31/12/2025"), false);
+        Todo t8 = new Todo(null, "Investimento", "Fazer investimentos a longo prazo", sdf.parse("31/12/2030"), false);
+        Todo t9 = new Todo(null, "Ajudar", "Ajudar meus pais com a reforma da casa", sdf.parse("26/02/2022"), false);
+        Todo t10 = new Todo(null, "Emagrecer", "Emagrecer para melhorar a saúde", sdf.parse("30/01/2022"), false);
 
-
-        todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4, t5, t6));
+        todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10)); 
 	}
 
 }
